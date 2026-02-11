71dd1a18653fc499c8504a32da44298aa4619b02
jabberwocky238 <7176656@qq.com>
2026-02-11 09:23:49 -0500

    ?

--- cmd/outer/main.go
+++ cmd/outer/main.go
@@ -41,7 +41,8 @@ func main() {
 	log.Println("Outer gateway starting...")
 
 	// Setup External Gin router (public access)
-	router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Recovery())
 	router.GET("/health", handlers.HealthOuter)
 	// 过滤 /health 请求的日志
 	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
